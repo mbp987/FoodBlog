4e998cda05daf36c2585998df6e163b18861bdd1
mbp987 <mbpodpora@yahoo.com>
2018-01-02 23:58:50 +0100

    empty skeleton

---
+++ src/test/java/pl/blog/FoodBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
