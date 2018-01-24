6f0919ff349019ecaae607c2e56d34987dc7b171
mbp987 <mbpodpora@yahoo.com>
2018-01-24 23:50:20 +0100

    security part

---
+++ src/main/java/pl/blog/security/AuthenticationComponent.java
@@ -0,0 +1,22 @@
+package pl.blog.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationComponent{
+	
+	public boolean isAnonymous() {
+		
+		//to be completed
+		return false;
+		
+	}
+
+	public boolean isAdmin() {
+		
+		//to be completed
+		return false;
+		
+	}
+
+}
